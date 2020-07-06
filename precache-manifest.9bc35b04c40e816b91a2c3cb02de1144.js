cc029a154e569aea94a95bd75334ec17b5f9c0ea
Shanshan zhou <zhoussneu@gmail.com>
2020-07-06 16:02:30 +1200

    Updates

---
+++ precache-manifest.9bc35b04c40e816b91a2c3cb02de1144.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa67488fd3620b9a83764b78fd56207f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99b3f1dc8e9d532577b9",
+    "url": "/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99b3f1dc8e9d532577b9",
+    "url": "/static/js/main.a4c2de94.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
